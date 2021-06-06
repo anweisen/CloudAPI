ef5bff9c633447d88789092022db4ab67ea647ab
anweisen <anweisen@web.de>
2021-06-06 22:35:04 +0200

    Remove EventPriority

--- api/driver/src/main/java/net/anweisen/cloudapi/driver/event/EventListener.java
+++ api/driver/src/main/java/net/anweisen/cloudapi/driver/event/EventListener.java
@@ -1,11 +1,17 @@
 package net.anweisen.cloudapi.driver.event;
 
 import javax.annotation.Nonnull;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * @author anweisen | https://github.com/anweisen
  * @since 1.0
  */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface EventListener {
 
 	/**
@@ -13,11 +19,5 @@ public @interface EventListener {
 	 */
 	@Nonnull
 	String channel() default "*";
-
-	/**
-	 * @return the priority of this listener, indicates the order, will translated to the cloud's priority system, will be ignored if not supported
-	 */
-	@Nonnull
-	EventPriority priority() default EventPriority.NORMAL;
 
 }
